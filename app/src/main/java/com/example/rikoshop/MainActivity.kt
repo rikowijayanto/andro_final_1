9248ba3dd8eb3dcd2be9d9adb2e507aa0970d0c4
Riko Wijayanto <mr.rikowijayanto@gmail.com>
2020-04-12 22:34:45 +0700

    Adding method to run and tested in staging

--- app/src/main/java/com/example/rikoshop/MainActivity.kt
+++ app/src/main/java/com/example/rikoshop/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.rikoshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvproduk: RecyclerView //deklarasi variabel produk
+    private var list: ArrayList<Produk> = arrayListOf() //mendefinisikan list
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvproduk = findViewById(R.id.rv_shop)
+        rvproduk.setHasFixedSize(true)
+
+
+        //memanggil kelas
+
+        list.addAll(ProdukData.listData)
+        showRecyclerList()
+    }
+
+
+    //memanggil function list
+    private fun showRecyclerList() {
+        rvproduk.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListProdukAdapter(list)
+        rvproduk.adapter = listHeroAdapter
     }
 }
